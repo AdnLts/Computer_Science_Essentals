2ac66f721f673a8d165d2634af8f54182c78e2a0
Aidan Litschi <162594337+AdnLts@users.noreply.github.com>
2024-03-07 10:18:10 -0500

    Create 1.3.3_MultiplicationCalculator.py

---
+++ Unit1/1.3.3_MultiplicationCalculator.py
@@ -0,0 +1,5 @@
+print("Welcome to the Simple Calculator!")
+print("This calculator takes two user supplied numbers and multiplies them.")
+NUM1=int(input("Enter the first number: "))
+NUM2=int(input("Enter the Second number: "))
+print(NUM1*NUM2)
