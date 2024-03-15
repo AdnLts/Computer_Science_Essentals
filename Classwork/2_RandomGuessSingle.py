2a0be10e30db13201ab8dd88f42e15025ebc3a8d
Aidan Litschi <162594337+AdnLts@users.noreply.github.com>
2024-03-15 09:05:41 -0400

    Update 2_RandomGuessSingle.py

--- Classwork/2_RandomGuessSingle.py
+++ Classwork/2_RandomGuessSingle.py
@@ -8,30 +8,72 @@ import random
 # Variables
 Player_Guess = -1
 Random_Number= -1
-Player_Lives = 5
-Win = 0
+Again = 1
 #----------------------------------------------------------------------------
 # GAME START
 print("Hello! Welcome to the Single Player Guessing Game!")
-print("Enter a number from 1-1000")
-Random_Number = random.randint(1,1000)
-#----------------------------------------------------------------------------
-#IF GAME
-while Player_Lives > 0:
-    print(Player_Lives)
-    Player_Guess = int(input("-> "))
-    if Player_Guess == Random_Number:
-        print("You Win!")
-        Player_Lives = 0
-        Win = 1
-    elif Player_Guess > Random_Number:
-        print("Guess Lower")
-        Player_Lives -= 1
-    elif Player_Guess < Random_Number:
-        print("Guess Higher")
-        Player_Lives -= 1
+
+while Again != 2:
+    Random_Number = random.randint(1,1000)
+    Player_Lives = 10
+    Win = 0
+
+    print("Enter a number from 1-1000")
+    print()
+
+    while Player_Lives > 0:
+        try:
+            print("Lives =", Player_Lives)
+            Player_Guess = int(input("-> "))
+
+            if Player_Guess == -7:
+                print(Random_Number)
+
+            elif Player_Guess == Random_Number:
+                print("You Win!")
+                Player_Lives = 0
+                Win = 1
+
+            elif Player_Guess > Random_Number:
+                print("Guess Lower")
+                print()
+                Player_Lives -= 1
+
+            elif Player_Guess < Random_Number:
+                print("Guess Higher")
+                print()
+                Player_Lives -= 1
+
+            else:
+                print("ERROR   >:(")
+                Player_Lives = 0
+        except:
+            print("An error has occured")
+            print()
+#---------------------------------------------------------------------------
+#End Game
+    if Win != 1:
+        print("YOU LOSE!")
+        print("The number was",Random_Number)
+        print()
+        print("Play Again?")
+        print("Yes (1), No (2)")
+        try:
+            Again = int(input("-> "))
+
+        except:
+            print("An error has occured")
+            break
+
     else:
-        print("ERROR   >:(")
-        Player_Lives = 0
-if Win != 1:
-    print("YOU LOSE!")
+        print()
+        print("Play Again?")
+        print("Yes (1), No (2)")
+        try:
+            Again = int(input("-> "))
+
+        except:
+            print("An error has occured")
+            break
+
+print("Thanks for playing!")
