70602de311f50ea25411874763e03a69a683477f
Aidan Litschi <162594337+AdnLts@users.noreply.github.com>
2024-04-02 08:56:30 -0400

    Update 3_DnDiceRoll.py

--- Classwork/3_DnDiceRoll.py
+++ Classwork/3_DnDiceRoll.py
@@ -3,14 +3,19 @@ import random
 DiceNum = -1
 Diceside = -1
 Roll = 0
+Total = 0
 #------------------------------
 print("Dice Roller")
-DiceNum = int(input("Number of dice: "))
-print()
-Diceside = int(input("Number of sides on dice: "))
-print()
-#------------------------------
-while DiceNum > 0:
-  Roll = random.randint(1,Diceside)
-  print("Roll "+str(DiceNum)+" is "+str(Roll))
-  DiceNum -= 1
+try:
+    DiceNum = int(input("Number of dice: "))
+    print()
+    Diceside = int(input("Number of sides on dice: "))
+    print()
+    while DiceNum > 0:
+        Roll = random.randint(1,Diceside)
+        print("Roll ",DiceNum," is ",Roll)
+        DiceNum -= 1
+        Total += Roll
+    print("The total is ",Total)
+except:
+    print("an error has occured")
