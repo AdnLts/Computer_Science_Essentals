fd3fa152744833b5df114f72eb88635e5bf425ee
Aidan Litschi <162594337+AdnLts@users.noreply.github.com>
2024-04-11 23:45:25 -0400

    Create 2.6_ThatsAlotOf_!_<--That.py

---
+++ Unit2/2.6_ThatsAlotOf_!_<--That.py
@@ -0,0 +1,13 @@
+print("Guess That Number!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+#Ignor the obsene amount of ! <--- that thing
+try:
+  UGuess = int(input("Guess a number ->"))
+  if UGuess == -7:
+    print("You Win!")
+    print(":D")
+  else:
+    print("You Lose!")
+    print(">:)")
+except:
+  print("How did you break this.")
+  print("It is litteraly just 13 lines of code")
