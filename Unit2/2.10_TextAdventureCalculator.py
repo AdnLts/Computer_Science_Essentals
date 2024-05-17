736b154d350a21dbd0498df67333bd86eb8d768b
Aidan Litschi <162594337+AdnLts@users.noreply.github.com>
2024-05-17 09:09:54 -0400

    Update and rename 2.10_TextAdventure to 2.10_TextAdventureCalculator.py

---
+++ Unit2/2.10_TextAdventureCalculator.py
@@ -0,0 +1,20 @@
+#Text Adventure Calculator
+#The only calculator with a 50% chance to not work
+
+import random
+
+#functions
+
+# Addition Function
+# Adds two user supplied number
+def Add(FailChance)
+
+
+
+
+
+
+#FailChance
+#Gives Chance to fail
+# 0 = Win, 1 = Fail
+FailChance = random.randint(0,1)
